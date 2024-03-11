1078d867fc783596f4f6d90cebed7b8754ccda94
Damitha Perera <damithap@99x.io>
2024-03-11 21:04:57 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   // app.enableCors();
-
+console.log('hhhh')
   const port = process.env.PORT || 3000;
   await app.listen(port); // 3000
 }
