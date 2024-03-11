123f5f1e6162e20555582730f0e9ab83325f8270
Damitha Perera <damithap@99x.io>
2024-03-11 20:23:01 +0530

    issue fixes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  // app.enableCors();
 
   const port = process.env.PORT || 3000;
   await app.listen(port); // 3000
