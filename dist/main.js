ed4b7c291e84c1f08ea4770da9145ff703d31b66
Damitha Perera <damithap@99x.io>
2024-03-12 19:17:12 +0530

    new changes 5

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,13 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    });
     app.useGlobalPipes(new common_1.ValidationPipe());
-    app.enableCors();
     const port = process.env.PORT || 3000;
     await app.listen(port);
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  // app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port); // 3000
 }
