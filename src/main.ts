05161632af42fd2bd9259aadbe6a495fa431f590
Damitha Perera <damithap@99x.io>
2024-03-12 18:18:23 +0530

    new changes

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://13.60.12.139',
+      'https://finlay-psi.vercel.app',
+      'https://finlay-backend.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   // app.enableCors();
-console.log('hhhh')
+  console.log('hhhh');
   const port = process.env.PORT || 3000;
   await app.listen(port); // 3000
 }
